1de53f832c3b37b85c00883aeaedb7ebaaf1ece1
Joel Ramos <joel.mariano.ramos@gmail.com>
2020-08-08 16:05:36 -0700

    update navigation test

--- .qawolf/navigation.test.js
+++ .qawolf/navigation.test.js
@@ -19,5 +19,9 @@ test("navigation", async () => {
   await page.goto("https://spacex-ships.now.sh/");
   await qawolf.scroll(page, "html", { x: 0, y: 3016 });
   await page.click('css=[aria-label="Pagination Navigation"] >> text=2');
-  await qawolf.scroll(page, "html", { x: 0, y: 0 });
+  // new stuff
+  const selector = 'css=[data-tid="ship-header--bettyrgambarella"]'
+  await page.waitForSelector(selector)
+  const shipName = await page.$eval(selector, el => el.innerText)
+  expect(shipName).toEqual('Betty R Gambarella')
 });
