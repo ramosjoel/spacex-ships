1e32d65fee32ba9a4362615b524b319c8750e2b3
Joel Ramos <joel.mariano.ramos@gmail.com>
2020-08-08 17:51:18 -0700

    recreate qawolf navigation test with data-tid attribute

--- .qawolf/navigation.test.js
+++ .qawolf/navigation.test.js
@@ -17,11 +17,6 @@ afterAll(async () => {
 
 test("navigation", async () => {
   await page.goto("https://spacex-ships.now.sh/");
-  await qawolf.scroll(page, "html", { x: 0, y: 3016 });
   await page.click('css=[aria-label="Pagination Navigation"] >> text=2');
-  // new stuff
-  const selector = 'css=[data-tid="ship-header--bettyrgambarella"]'
-  await page.waitForSelector(selector)
-  const shipName = await page.$eval(selector, el => el.innerText)
-  expect(shipName).toEqual('Betty R Gambarella')
+  await page.click('[data-tid="ship-header--bettyrgambarella"]');
 });
--- qawolf.config.js
+++ qawolf.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  attribute: 'data-tid',
   config: "node_modules/qawolf/js-jest.config.json",
   rootDir: ".qawolf",
   testTimeout: 60000,
